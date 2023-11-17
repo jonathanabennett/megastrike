eacf93e10bc121d3e603169182ba473b003bb2c5
Jonathan Bennett <jonathanbennett@boromir.local>
2023-11-17 19:30:23 +0900

    Ported Hexagon library from Common Lisp

---
+++ src/megastrike/hexagons/hex.clj
@@ -0,0 +1,152 @@
+(ns megastrike.hexagons.hex
+  (:require [clojure.math :as math]))
+
+(defn hexagon
+  ([q r]
+   (hexagon q r (* (+ q r) -1)))
+  ([q r s]
+   (when (= (* (+ q r) -1) s)
+      {:q q :r r :s s})))
+
+(defn hex-from-offset
+  ([col row]
+  (let [q col
+        r (int (- row (math/floor (/ (+ col (* (mod (abs col) 2) -1)) 2))))
+        s (int (* (+ q r) -1))]
+    (hexagon q r s)))
+  ([offset]
+   (hex-from-offset (:x offset) (:y offset))))
+
+(defn offset-from-hex
+  [hex]
+  (let [row (int (+ (:r hex) (math/floor (/ (+ (:q hex) (* (mod (abs (:q hex)) 2) -1)) 2))))
+        col (:q hex)]
+    {:x col :y row}))
+
+(defn same-hex
+  [hex1 hex2]
+  (and (= (:q hex1) (:q hex2))
+       (= (:r hex1) (:r hex2))
+       (= (:s hex1) (:s hex2))))
+
+(defn hex-addition
+  "Use Cartesian addition to add two hexagons together."
+  [hex1 hex2]
+  (hexagon (+ (:q hex1) (:q hex2)) (+ (:r hex1) (:r hex2)) (+ (:s hex1) (:s hex2))))
+
+(defn hex-subtraction
+  "Use Cartesian subtraction to add two hexagons together."
+  [hex1 hex2]
+  (hexagon (- (:q hex1) (:q hex2)) (- (:r hex1) (:r hex2)) (- (:s hex1) (:s hex2))))
+
+(defn hex-multiplication
+  "Use Cartesian multiplication to multipy a hex by a value x."
+  [hex x]
+  (hexagon (* (:q hex) x) (* (:r hex) x) (* (:s hex) x)))
+
+(defn hex-distance
+  [hex1 hex2]
+  (let [length (hex-subtraction hex1 hex2)]
+    (/ (+ (abs (:q length)) (abs (:r length)) (abs (:s length))) 2)))
+
+(def hex-ordinals (list {:q 1  :r 0  :s -1}
+                        {:q 1  :r -1 :s 0}
+                        {:q 0  :r -1 :s 1}
+                        {:q -1 :r 0  :s 1}
+                        {:q -1 :r 0  :s 1}
+                        {:q 0  :r 1  :s -1}))
+
+(defn hex-direction
+  "Returns the coordinate transformation to select a hex in a given direction"
+  [direction]
+  (let [dir (mod direction 6)]
+    (nth hex-ordinals direction)))
+
+(defn hex-neighbor
+  [hex direction]
+  (hex-addition hex (hex-direction direction)))
+
+(defn create-layout
+  []
+  {:hex-to-pixel-matrix [(/ 3.0 2.0) 0 (/ (math/sqrt 3.0) 2.0) (math/sqrt 3.0)]
+   :pixel-to-hex-matrix [(/ 2.0 3.0) 0 (/ -1.0 3.0) (/ (math/sqrt 3.0) 3.0)]
+   :start-angle 0
+   :x-size 84
+   :y-size 72
+   :x-origin 84
+   :y-origin 65})
+
+(defn hex-to-pixel
+  [hex layout]
+  (let [htp (:hex-to-pixel-matrix layout)]
+    {:x (+ (* (+ (* (get htp 0) (:q hex))
+                 (* (get htp 1) (:r hex)))
+              (:x-size layout))
+           (:x-origin layout))
+     :y (+ (* (+ (* (get htp 2) (:q hex))
+                 (* (get htp 3) (:r hex)))
+              (:y-size layout))
+           (:y-origin layout))}))
+
+(defn hex-round
+  [q r s]
+  (let [q-int (math/round q)
+        r-int (math/round r)
+        s-int (math/round s)
+        q-diff (abs (- q q-int))
+        r-diff (abs (- r r-int))
+        s-diff (abs (- s s-int))]
+    (cond
+      (and (> q-diff r-diff)
+            (> q-diff s-diff))
+       (hexagon (* (+ r-int s-int) -1) r-int s-int)
+      (and (> r-diff s-diff)
+            (> r-diff q-diff))
+       (hexagon q-int (* (+ q-int s-int) -1) s-int)
+      :else (hexagon q-int r-int (* (+ q-int r-int) -1)))))
+
+(defn pixel-to-hex
+  [pt layout]
+  (let [pth (:pixel-to-hex-matrix layout)
+        modified-point {:x (/ (- (:x pt)
+                                 (:x-origin layout))
+                              (:x-size layout))
+                        :y (/ (- (:y pt)
+                                 (:y-origin layout))
+                              (:y-size layout))}
+        q (+ (* (:x modified-point) (get pth 0))
+             (* (:y modified-point) (get pth 1)))
+        r (+ (* (:x modified-point) (get pth 2))
+             (* (:y modified-point) (get pth 3)))]
+    (hex-round q r (* (+ q r) -1))))
+
+(defn find-hex-corner
+  [center corner layout]
+  (let [angle (* 2.0 math/PI (/ (+ (:start-angle layout) corner) 6))]
+    {:x (+ (* (:x-size layout)
+              (math/cos angle))
+           (:x center))
+     :y (+ (* (:y-size layout)
+              (math/sin angle))
+           (:y center))}))
+
+(defn hex-points
+  [hex layout]
+  (let [center (hex-to-pixel hex layout)]
+    (map #(find-hex-corner center % layout) (list 0 1 2 3 4 5))))
+
+(defn linear-interpolation
+  [a b step]
+  (+ a (* (- b a) step)))
+
+(defn hex-lerp
+  [hex1 hex2 step]
+  {:q (linear-interpolation (:q hex1) (:q hex2) step)
+   :r (linear-interpolation (:r hex1) (:r hex2) step)
+   :s (linear-interpolation (:s hex1) (:s hex2) step)})
+
+(defn hex-line
+  [hex1 hex2]
+  (let [distance (hex-distance hex1 hex2)
+        step (/ 1.0 (max distance 1))]
+    [{:q 0 :r 0 :s 0}]))
---
+++ test/megastrike/hexagons/hex_test.clj
@@ -0,0 +1,110 @@
+(ns megastrike.hexagons.hex-test
+  (:require [megastrike.hexagons.hex :as sut]
+            [clojure.test :as t]))
+
+
+(t/deftest hexagon-equality
+  (t/testing "Two hexes should match if they have the same q,r,s address."
+    (t/is (= (sut/same-hex (sut/hexagon 0 0 0) (sut/hexagon 0 0 0)) true)))
+  (t/testing "Two hexes should NOT match if they have different q,r,s addresses."
+    (t/is (= (sut/same-hex (sut/hexagon 0 0 0) (sut/hexagon 0 -1 1)) false)))
+  (t/testing "s = -q-r in hex creation."
+    (t/is (= (sut/same-hex (sut/hexagon 1 0) (sut/hexagon 1 0 -1)) true)))
+  (t/testing "Disparate maps should not match."
+    (t/is (= (sut/same-hex (sut/hexagon 0 0 0) {:x 0 :y 0}) false))))
+
+(t/deftest hex-addition
+  (let [origin-hex (sut/hexagon 0 0 0)
+        diff-q-hex (sut/hexagon 1 0 -1)
+        diff-r-hex (sut/hexagon 0 1 -1)
+        diff-s-hex (sut/hexagon -1 -1 2)]
+    (t/testing "Testing hexagon addition."
+      (t/is (sut/same-hex (sut/hex-addition origin-hex diff-q-hex)
+                         (sut/hexagon 1 0 -1)))
+      (t/is (sut/same-hex (sut/hex-addition origin-hex diff-r-hex)
+                         (sut/hexagon 0 1 -1)))
+      (t/is (sut/same-hex (sut/hex-addition origin-hex diff-s-hex)
+                         (sut/hexagon -1 -1 2)))
+      (t/is (sut/same-hex (sut/hex-addition diff-q-hex diff-r-hex)
+                         (sut/hexagon 1 1 -2))))))
+
+(t/deftest hex-subtraction
+  (let [origin-hex (sut/hexagon 0 0 0)
+        diff-q-hex (sut/hexagon 1 0 -1)
+        diff-r-hex (sut/hexagon 0 1 -1)
+        diff-s-hex (sut/hexagon -1 -1 2)]
+    (t/testing "Testing hexagon subtraction."
+      (t/is (sut/same-hex (sut/hex-subtraction origin-hex diff-q-hex)
+                         (sut/hexagon -1 0 1)))
+      (t/is (sut/same-hex (sut/hex-subtraction origin-hex diff-r-hex)
+                         (sut/hexagon 0 -1 1)))
+      (t/is (sut/same-hex (sut/hex-subtraction origin-hex diff-s-hex)
+                         (sut/hexagon 1 1 -2)))
+      (t/is (sut/same-hex (sut/hex-subtraction diff-q-hex diff-r-hex)
+                         (sut/hexagon 1 -1 0))))))
+
+(t/deftest hex-offset-conversion
+  (let [test-hex-00 (sut/hexagon 0 0 0)
+        test-hex-offset-00 {:x 0 :y 0}
+        test-hex-42 (sut/hexagon 4 0 -4)
+        test-hex-offset-42 {:x 4 :y 2}]
+    (t/testing "Testing from hex to offset."
+      (t/is (= test-hex-offset-00 (sut/offset-from-hex test-hex-00)))
+      (t/is (not (= test-hex-offset-00 (sut/offset-from-hex test-hex-42))))
+      (t/is (= test-hex-offset-42 (sut/offset-from-hex test-hex-42)))
+      (t/is (not (= test-hex-offset-42 (sut/offset-from-hex test-hex-00)))))
+    (t/testing "Testing hex from offset."
+      (t/is (= test-hex-00 (sut/hex-from-offset test-hex-offset-00)))
+      (t/is (not (= test-hex-00 (sut/hex-from-offset test-hex-offset-42))))
+      (t/is (= test-hex-42 (sut/hex-from-offset test-hex-offset-42)))
+      (t/is (not (= test-hex-42 (sut/hex-from-offset test-hex-offset-00)))))))
+
+(t/deftest hex-multiplication
+  (let [test-hex (sut/hexagon 4 0 -4)]
+    (t/testing "Testing hex multiplication"
+      (t/is (= (sut/hex-multiplication test-hex 2) {:q 8 :r 0 :s -8})))))
+
+(t/deftest hex-distance
+  (let [test-hex-00 (sut/hexagon 0 0 0)
+        test-hex-42 (sut/hexagon 4 0 -4)]
+    (t/testing "Measure distance from hex 0,0 to hex 2,4"
+      (t/is (= (sut/hex-distance test-hex-00 test-hex-42) 4)))))
+
+(t/deftest hex-neighbor
+  (let [test-hex-00 (sut/hexagon 0 0 0)]
+    (t/testing "Check each hex neighbor for hex 0,0"
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 0) {:q 1 :r 0 :s -1}))
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 1) {:q 1 :r -1 :s 0}))
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 2) {:q 0 :r -1 :s 1}))
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 3) {:q -1 :r 0 :s 1}))
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 4) {:q -1 :r 0 :s 1}))
+      (t/is (sut/same-hex (sut/hex-neighbor test-hex-00 5) {:q 0 :r 1 :s -1})))))
+
+(t/deftest hex-to-pixel
+  (let [layout (sut/create-layout)]
+    (t/testing "Check the pixel location of hexes."
+      (let [test-hex-00 (sut/hexagon 0 0 0)
+            test-hex-42 (sut/hexagon 4 0 -4)]
+        (t/is (= (sut/hex-to-pixel test-hex-00 layout) {:x 84.0 :y 65.0}))
+        (t/is (= (sut/hex-to-pixel test-hex-42 layout) {:x 588.0 :y 314.41531628991834}))))))
+
+(t/deftest pixel-to-hex
+  (let [layout (sut/create-layout)]
+    (t/testing "Check the hex location of pixels."
+      (let [test-hex-00 (sut/hexagon 0 0 0)
+            test-hex-42 (sut/hexagon 4 0 -4)]
+        (t/is (= (sut/pixel-to-hex {:x 100.0 :y 30.0} layout) (sut/hexagon 0 0 0)))
+        (t/is (= (sut/pixel-to-hex {:x 55.0 :y 55.0} layout) (sut/hexagon 0 0 0)))
+        (t/is (= (sut/pixel-to-hex {:x 55.0 :y 45.0} layout) (sut/hexagon 0 0 0)))))))
+
+(t/deftest hex-points
+  (let [layout (sut/create-layout)]
+    (t/testing "Find all vertices of a hex"
+      (let [test-hex-00 (sut/hexagon 0 0 0)]
+        (t/is (= (sut/hex-points test-hex-00 layout)
+                 (list {:x 168.0, :y 65.0}
+                       {:x 125.99999999999997, :y 127.3538290724796}
+                       {:x 42.00000000000002, :y 127.3538290724796}
+                       {:x 0.0, :y 65.00000000000001}
+                       {:x 42.000000000000036, :y 2.6461709275204015}
+                       {:x 125.99999999999994, :y 2.6461709275203873})))))))
