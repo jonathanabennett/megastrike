b485df8a2c17a05904054e91028cb4c615b84159
Jonathan Bennett <jonathanbennett@boromir.local>
2023-11-19 21:34:59 +0900

    Board Parsing
    To supplement Hexagon library

---
+++ src/megastrike/board.clj
@@ -0,0 +1,36 @@
+(ns megastrike.board
+  (:require [megastrike.hexagons.hex :as hex]
+            [clojure.string :as str]))
+
+(defn create-tile
+  ([q r s elevation terrain palette]
+   (create-tile (hex/hexagon q r s) elevation terrain palette))
+  ([x y elevation terrain palette]
+   (create-tile (hex/hex-from-offset x y) elevation terrain palette))
+  ([hex elevation terrain palette]
+   (merge hex {:elevation elevation :terrain terrain :palette palette})))
+
+(defn strip-quotes
+  [str]
+  (str/replace str #"\"" ""))
+
+(defn parse-hex-line
+  [line]
+  (let [line-str (str/split line #" ")
+        x (Integer/parseInt (subs (nth line-str 1) 0 2))
+        y (Integer/parseInt (subs (nth line-str 1) 2 4))
+        elevation (Integer/parseInt (nth line-str 2))
+        terrain (nth line-str 3)
+        style (nth line-str 4)]
+    (create-tile x y elevation (strip-quotes terrain) (strip-quotes style))))
+
+(defn create-board
+  ([filename]
+   (let [f (slurp filename)]
+     (vec (remove nil?
+                  (for [line (str/split-lines f)]
+                    (when (str/includes? line "hex")
+                      (parse-hex-line line)))))))
+  ([width height]
+   (vec (for [x (vec (range 1 (inc width))) y (vec (range 1 (inc height)))]
+         (create-tile x y 0 "" "grass")))))
