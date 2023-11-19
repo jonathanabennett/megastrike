0d2c5f9dc107163809056f0f530cbc60594f17b4
Jonathan Bennett <jonathanbennett@boromir.local>
2023-11-19 21:34:27 +0900

    Game Atom development

---
+++ src/megastrike/game.clj
@@ -0,0 +1,60 @@
+(ns megastrike.game
+  (:require [megastrike.combat-unit :as unit]
+            [clojure.string :as str]))
+
+(def empty-game
+  {:forces {}
+   :units {}
+   :active-unit nil
+   :active-force nil
+   :game-board []
+   :current-phase -1
+   :turn-number 0})
+
+(defonce *game
+  (atom empty-game))
+
+(defn new-game
+  []
+  (reset! *game empty-game))
+
+(defn get-forces
+  []
+  (:forces @*game))
+
+(defn add-force!
+  [{name :name :as force}]
+  (let [forces (get-forces)]
+    (swap! *game #(assoc % :forces (assoc forces name force)))))
+
+(defn del-force!
+  [name]
+  (let [forces (get-forces)]
+    (swap! *game #(assoc % :forces (dissoc forces name)))))
+
+(defn get-force
+  [name]
+  (get (get-forces) name))
+
+(defn get-units
+  []
+  (:units @*game))
+
+(defn add-unit!
+  [unit]
+  (let [units (get-units)
+        full-name (:full-name unit)
+        counter (count (remove #(not (str/includes? (first %) full-name)) units))
+        id (if (= counter 0)
+             (:full-name unit)
+             (str full-name " #" (inc counter)))]
+    (swap! *game #(assoc % :units (assoc units id unit)))))
+
+(defn del-unit!
+  [id]
+  (let [units (get-units)]
+    (swap! *game #(remove (fn [u-id] (= u-id id)) units))))
+
+(defn get-unit
+  [id]
+  (get (get-units) id))
