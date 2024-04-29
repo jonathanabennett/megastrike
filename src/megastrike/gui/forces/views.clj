aa3364cd305164c8abaf34b26d99b9b5e0647fd0
Jonathan Bennett <doulos05@gmail.com>
2024-04-29 10:17:21 +0900

    Unit Stats are now an accordian grouped by force.

--- src/megastrike/gui/forces/views.clj
+++ src/megastrike/gui/forces/views.clj
@@ -10,67 +10,75 @@
   (let [background (if (= (:id unit) (fx/sub-val context :active-unit))
                      "-fx-background-color: #AAAAAA;"
                      "-fx-background-color: #DDDDDD;")]
-    {:fx/type :v-box
-     :border {:strokes [{:stroke :black :style :solid :widths 2}]}
-     :style background
+    {:fx/type :titled-pane
+     :text (:id unit)
+     :border {:strokes [{:stroke :black :style :solid :width 2}]}
      :padding 5
-     :spacing 5
-     :on-mouse-clicked {:event-type ::events/stats-clicked :fx/sync true :unit (:id unit)}
-     :children [{:fx/type :h-box
-                 :spacing 5
-                 :children [{:fx/type common/prop-label
-                             :label "Unit: "
-                             :value (:id unit)}
-                            {:fx/type common/prop-label
-                             :label "Force: "
-                             :value (-> unit :force name str/capitalize)}
-                            {:fx/type common/prop-label
-                             :label "Type: "
-                             :value (:type unit)}
-                            {:fx/type common/prop-label
-                             :label "Mv: "
-                             :value (cu/print-movement unit)}]}
-                {:fx/type :h-box
-                 :spacing 5
-                 :children [{:fx/type common/prop-label
-                             :label "Role: "
-                             :value (:role unit)}
-                            {:fx/type common/prop-label
-                             :label "Size: "
-                             :value (str (:size unit))}
-                            {:fx/type common/prop-label
-                             :label "TMM: "
-                             :value (str (:tmm unit))}]}
-                {:fx/type common/prop-label
-                 :label "Pilot (skill): "
-                 :value (str (:name (:pilot unit)) " (" (:skill (:pilot unit)) ")")}
-                {:fx/type common/attack-table
-                 :unit unit}
-                { :fx/type common/draw-pips 
-                 :text (str "Armor: " (:current-armor unit) "/" (:armor unit)) 
-                 :filled (:current-armor unit)
-                 :max (:armor unit)
-                 :fill-one :green
-                 :fill-two :transparent}
-                {:fx/type common/draw-pips 
-                 :text (str "Structure: " (:current-structure unit) "/" (:structure unit))
-                 :filled (:current-structure unit)
-                 :max (:structure unit)
-                 :fill-one :green
-                 :fill-two :transparent}
-                {:fx/type common/draw-pips
-                 :text (str "Heat: " (:current-heat unit) "/" 4)
-                 :filled (:current-heat unit)
-                 :max 4
-                 :fill-one :red 
-                 :fill-two :aliceblue} ]}))
+     :content {:fx/type :v-box 
+               :style background
+               :spacing 5 
+               :on-mouse-clicked {:event-type ::events/stats-clicked :fx/sync true :unit (:id unit)} 
+               :children [{:fx/type :h-box 
+                           :spacing 5 
+                           :children [{:fx/type common/prop-label 
+                                       :label "Unit: " 
+                                       :value (:id unit)} 
+                                      {:fx/type common/prop-label 
+                                       :label "Force: " 
+                                       :value (-> unit :force name str/capitalize)} 
+                                      {:fx/type common/prop-label 
+                                       :label "Type: " 
+                                       :value (:type unit)} 
+                                      {:fx/type common/prop-label 
+                                       :label "Mv: " 
+                                       :value (cu/print-movement unit)}]} 
+                          {:fx/type :h-box
+                           :spacing 5 
+                           :children [{:fx/type common/prop-label 
+                                       :label "Role: " 
+                                       :value (:role unit)} 
+                                      {:fx/type common/prop-label 
+                                       :label "Size: " 
+                                       :value (str (:size unit))} 
+                                      {:fx/type common/prop-label 
+                                       :label "TMM: " 
+                                       :value (str (:tmm unit))}]} 
+                          {:fx/type common/prop-label 
+                           :label "Pilot (skill): " 
+                           :value (str (:name (:pilot unit)) " (" (:skill (:pilot unit)) ")")} 
+                          {:fx/type common/attack-table 
+                           :unit unit} 
+                          { :fx/type common/draw-pips 
+                           :text (str "Armor: " (:current-armor unit) "/" (:armor unit)) 
+                           :filled (:current-armor unit) 
+                           :max (:armor unit) 
+                           :fill-one :green 
+                           :fill-two :transparent} 
+                          {:fx/type common/draw-pips 
+                           :text (str "Structure: " (:current-structure unit) "/" (:structure unit)) 
+                           :filled (:current-structure unit) 
+                           :max (:structure unit) 
+                           :fill-one :green 
+                           :fill-two :transparent} 
+                          {:fx/type common/draw-pips 
+                           :text (str "Heat: " (:current-heat unit) "/" 4) 
+                           :filled (:current-heat unit) 
+                           :max 4 
+                           :fill-one :red 
+                           :fill-two :aliceblue}]}}))
 
-(defn force-block [{:keys [units]}]
-  {:fx/type :v-box
-   :spacing 15
-   :border {:strokes [{:stroke (:color (:force (first units))) :style :solid :widths 2}]}
-   :children (for [u units]
-               {:fx/type unit-stat-block :unit u})})
+(defn force-block [{:keys [fx/context units]}]
+  (let [forces (fx/sub-val context :forces)
+        force ((:force (first units)) forces)]
+    (prn force)
+    {:fx/type :v-box
+     :spacing 5
+     :border {:strokes [{:stroke (:color force) :style :solid :width 5}]}
+     :children [{:fx/type :label
+                 :text (force :name)} 
+                {:fx/type :accordion 
+                 :panes (for [u units] 
+                          {:fx/type unit-stat-block :unit u})}]}))
 
 (defn stat-blocks [{:keys [fx/context]}]
   (let [units (group-by :force (vals (fx/sub-val context :units)))]
