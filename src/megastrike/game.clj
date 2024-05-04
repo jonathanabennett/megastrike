9eb1a6d1b5a83a08ee9627864ccc3806e2da9ffa
Jonathan Bennett <doulos05@gmail.com>
2024-05-04 18:39:47 +0900

    Remove code from game.clj from pre-cljfx

--- src/megastrike/game.clj
+++ src/megastrike/game.clj
@@ -11,50 +11,23 @@
    :current-phase -1
    :turn-number 0})
 
-(defonce *game
-  (atom empty-game))
-
 (defn new-game
   []
   (reset! *game empty-game))
 
-(defn get-forces
-  []
-  (:forces @*game))
+(defn phase-name
+  [n]
+  (case n
+    [-1 "Game Over"]
+    [0 "Initiative Phase"]
+    [1 "Deployment Phase"]
+    [2 "Movement Phase"]
+    [3 "Combat Phase"]
+    [4 "End Phase"]))
 
-(defn add-force!
-  [{name :name :as force}]
-  (let [forces (get-forces)]
-    (swap! *game #(assoc % :forces (assoc forces name force)))))
-
-(defn del-force!
-  [name]
-  (let [forces (get-forces)]
-    (swap! *game #(assoc % :forces (dissoc forces name)))))
-
-(defn get-force
-  [name]
-  (get (get-forces) name))
-
-(defn get-units
-  []
-  (:units @*game))
-
-(defn add-unit!
-  [unit]
-  (let [units (get-units)
-        full-name (:full-name unit)
-        counter (count (remove #(not (str/includes? (first %) full-name)) units))
-        id (if (= counter 0)
-             (:full-name unit)
-             (str full-name " #" (inc counter)))]
-    (swap! *game #(assoc % :units (assoc units id unit)))))
-
-(defn del-unit!
-  [id]
-  (let [units (get-units)]
-    (swap! *game #(assoc % :units (dissoc units id)))))
-
-(defn get-unit
-  [id]
-  (get (get-units) id))
+(defn next-phase
+  [phase]
+  (if (= 5 inc (:current-phase phase))
+    {:current-phase 0}
+    {:current-phase (inc (:current-phase phase))
+     :turn-number (inc (:turn-number phase))}))
