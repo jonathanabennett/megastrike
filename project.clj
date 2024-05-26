34983d1160d54142c3576ec88e3378052d12d71b
Jonathan Bennett <doulos05@gmail.com>
2024-05-27 08:16:54 +0900

    Replace version with empty string

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject megastrike
+(defproject megastrike ""
   :description "This is an implementation of the Alpha Strike board game for the computer."
   :url "http://example.com/FIXME"
   :plugins [[lein-cloverage "1.2.2"]]
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [net.sekao/odoyle-rules "1.3.1"]
