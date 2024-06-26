8353b8fe08b7ef0bf1c14aca2cd9f83f7e72c16b
Jonathan A. Bennett <doulos05@gmail.com>
2024-06-27 03:48:13 +0900

    Correct a duplicated hex-ordinal entry

--- src/megastrike/hexagons/hex.clj
+++ src/megastrike/hexagons/hex.clj
@@ -69,7 +69,7 @@
         {:p 1  :q -1 :r 0} 
         {:p 0  :q -1 :r 1} 
         {:p -1 :q 0  :r 1} 
-        {:p -1 :q 0  :r 1} 
+        {:p -1 :q 1  :r 0} 
         {:p 0  :q 1  :r -1}))
 
 (defn hex-direction
