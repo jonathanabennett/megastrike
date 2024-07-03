bcaa3330d0dc74590472b576caf04ed90ff526b1
Jonathan A. Bennett <doulos05@gmail.com>
2024-07-04 07:46:23 +0900

    Make turn button work in deployment phase

--- src/megastrike/gui/board/events.clj
+++ src/megastrike/gui/board/events.clj
@@ -15,16 +15,16 @@
          unit (subs/active-unit context) 
          next-force (first (subs/turn-order context))] 
      (cond 
-       (and (= phase :deployment) (not (nil? unit)) (not (get unit :acted)) (= (get unit :force) next-force)) 
-       (let [updated (merge unit (select-keys hex [:p :q :r])) 
-             new-units (assoc units active updated)] 
-         {:context (fx/swap-context context assoc :units new-units)})
-       (and (= phase :movement) (not (nil? unit)) (= (fx/sub-val context :turn-flag) true))
+       (and (some #(= phase %) [:deployment :movement]) (not (nil? unit)) (= (fx/sub-val context :turn-flag) true))
        (let [e ^MouseEvent event 
              u (if (:path unit) (last (:path unit)) unit)
              dest {:x (.getX e) :y (.getY e)}
              facing (hexagon/hex-facing u dest (subs/layout context))]
          {:dispatch {:event-type ::events/change-facing :unit unit :facing facing}})
+       (and (= phase :deployment) (not (nil? unit)) (not (get unit :acted)) (= (get unit :force) next-force)) 
+       (let [updated (merge unit (select-keys hex [:p :q :r])) 
+             new-units (assoc units active updated)] 
+         {:context (fx/swap-context context assoc :units new-units)})
        (and (= phase :movement) (not (nil? unit)) (not (get unit :acted)) (= (get unit :force) next-force)) 
        (let [updated (assoc unit :path (cu/find-path unit hex (subs/board context)))
              new-units (assoc units active updated)]
