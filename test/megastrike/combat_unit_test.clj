a7bbf384f7fcbc4d4813f7c4ce114b7dc7024da6
Jonathan A. Bennett <doulos05@gmail.com>
2024-07-07 05:19:22 +0900

    Expand and update tests

--- test/megastrike/combat_unit_test.clj
+++ test/megastrike/combat_unit_test.clj
@@ -4,7 +4,9 @@
             [clojure.test :as t]))
 
 (t/deftest test-move-parser
-  (t/testing "Valid Mv Strings"
+  (t/testing "Valid Mv Strings" 
+    (t/is (= (sut/parse-movement "8\\\"") {:walk 4}))
+    (t/is (= (sut/parse-movement "8\\\"j") {:jump 4 :walk 4}))
     (t/is (= (sut/parse-movement "6\\\"t") {:t 3}))
     (t/is (= (sut/parse-movement "16\\\"/12\\\"j") {:walk 8 :jump 6}))))
 
@@ -26,21 +28,21 @@
       (t/is (= (sut/construct-ability-list ba-test) [:am :car4 :mec :rcn :rsd1])))))
 
 (t/deftest test-mul-parser
-  (let [bm-test "73	Archer	ARC-2K	Missile Boat	BM	3	8\"	1	6	6	-1	2	FALSE	2	FALSE	2	FALSE	0	FALSE	2	34	IF2"
-        cv-test "4879	Puma Assault Tank	PAT-001	None	CV	4	6\"t	1	6	5	-1	4	FALSE	5	FALSE	3	FALSE	0	FALSE	0	37	IF2, LRM1/2/2, REAR1/-/-, SRCH, TUR(1/1/1)"
-        sv-test "3684	Air Car		None	SV	2	22\"h	4	1	2	-1	0	FALSE	0	FALSE	0	FALSE	0	FALSE	0	6	BAR, EE, ENE"
-        pm-test "510	Centaur	2	Scout	PM	1	12\"	2	1	1	-1	1	FALSE	0	TRUE	0	FALSE	0	FALSE	0	10					"
-        ci-test "-1	Motorized Sub Platoon	(Laser SCUBA)	Undetermined	CI	1	4\"s	0	1	1	-1	1	FALSE	1	FALSE	0	FALSE	0	FALSE	0	8	AM, CAR4, UMU				"
-        im-test "4545	Lumberjack	LM1/A	Ambusher	IM	3	6\"	1	1	5	-1	0	FALSE	0	FALSE	0	FALSE	0	FALSE	0	7	BAR, BFC, CT8, EE, ENE, MEL, SAW				"
-        ba-test "952	Elemental Battle Armor	(Headhunter)(Sqd4)	Ambusher	BA	1	6\"j	1	1	2	-1	1	FALSE	0	FALSE	0	FALSE	0	FALSE	0	15	AM, CAR4, MEC, RCN, RSD1				"]
+  (let [bm-test "73	Archer	ARC-2K	Missile Boat	BM	3	8\\\"	1	6	6	-1	2	FALSE	2	FALSE	2	FALSE	0	FALSE	2	34	IF2"
+        cv-test "4879	Puma Assault Tank	PAT-001	None	CV	4	6\\\"t	1	6	5	-1	4	FALSE	5	FALSE	3	FALSE	0	FALSE	0	37	IF2, LRM1/2/2, REAR1/-/-, SRCH, TUR(1/1/1)"
+        sv-test "3684	Air Car		None	SV	2	22\\\"h	4	1	2	-1	0	FALSE	0	FALSE	0	FALSE	0	FALSE	0	6	BAR, EE, ENE"
+        pm-test "510	Centaur	2	Scout	PM	1	12\\\"	2	1	1	-1	1	FALSE	0	TRUE	0	FALSE	0	FALSE	0	10					"
+        ci-test "-1	Motorized Sub Platoon	(Laser SCUBA)	Undetermined	CI	1	4\\\"s	0	1	1	-1	1	FALSE	1	FALSE	0	FALSE	0	FALSE	0	8	AM, CAR4, UMU				"
+        im-test "4545	Lumberjack	LM1/A	Ambusher	IM	3	6\\\"	1	1	5	-1	0	FALSE	0	FALSE	0	FALSE	0	FALSE	0	7	BAR, BFC, CT8, EE, ENE, MEL, SAW				"
+        ba-test "952	Elemental Battle Armor	(Headhunter)(Sqd4)	Ambusher	BA	1	6\\\"j	1	1	2	-1	1	FALSE	0	FALSE	0	FALSE	0	FALSE	0	15	AM, CAR4, MEC, RCN, RSD1				"]
     (t/testing "Sample MUL rows"
-      (t/is (= (sut/parse-row (first (csv/parse-csv bm-test :delimiter \tab))) {:role "Missile Boat", :tmm 1, :e* false, :movement {}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K"}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv cv-test :delimiter \tab))) {:role "None", :tmm 1, :e* false, :movement {}, :mul-id 4879, :l* false, :m 5, :type "CV", :abilities "IF2, LRM1/2/2, REAR1/-/-, SRCH, TUR(1/1/1)", :e 0, :s 4, :threshold -1, :l 3, :size 4, :m* false, :point-value 37, :overheat 0, :chassis "Puma Assault Tank", :structure 5, :full-name "Puma Assault Tank PAT-001", :armor 6, :s* false, :model "PAT-001"}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv sv-test :delimiter \tab))) {:role "None", :tmm 4, :e* false, :movement {}, :mul-id 3684, :l* false, :m 0, :type "SV", :abilities "BAR, EE, ENE", :e 0, :s 0, :threshold -1, :l 0, :size 2, :m* false, :point-value 6, :overheat 0, :chassis "Air Car", :structure 2, :full-name "Air Car ", :armor 1, :s* false, :model ""}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv pm-test :delimiter \tab))) {:role "Scout", :tmm 2, :left-arc "", :e* false, :movement {}, :right-arc "", :mul-id 510, :l* false, :m 0, :type "PM", :front-arc "", :abilities "", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* true, :rear-arc "", :point-value 10, :overheat 0, :chassis "Centaur", :structure 1, :full-name "Centaur 2", :armor 1, :s* false, :model "2"}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv ci-test :delimiter \tab))) {:role "Undetermined", :tmm 0, :left-arc "", :e* false, :movement {}, :right-arc "", :mul-id -1, :l* false, :m 1, :type "CI", :front-arc "", :abilities "AM, CAR4, UMU", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* false, :rear-arc "", :point-value 8, :overheat 0, :chassis "Motorized Sub Platoon", :structure 1, :full-name "Motorized Sub Platoon (Laser SCUBA)", :armor 1, :s* false, :model "(Laser SCUBA)"}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv im-test :delimiter \tab))) {:role "Ambusher", :tmm 1, :left-arc "", :e* false, :movement {}, :right-arc "", :mul-id 4545, :l* false, :m 0, :type "IM", :front-arc "", :abilities "BAR, BFC, CT8, EE, ENE, MEL, SAW", :e 0, :s 0, :threshold -1, :l 0, :size 3, :m* false, :rear-arc "", :point-value 7, :overheat 0, :chassis "Lumberjack", :structure 5, :full-name "Lumberjack LM1/A", :armor 1, :s* false, :model "LM1/A"}))
-      (t/is (= (sut/parse-row (first (csv/parse-csv ba-test :delimiter \tab))) {:role "Ambusher", :tmm 1, :left-arc "", :e* false, :movement {}, :right-arc "", :mul-id 952, :l* false, :m 0, :type "BA", :front-arc "", :abilities "AM, CAR4, MEC, RCN, RSD1", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* false, :rear-arc "", :point-value 15, :overheat 0, :chassis "Elemental Battle Armor", :structure 2, :full-name "Elemental Battle Armor (Headhunter)(Sqd4)", :armor 1, :s* false, :model "(Headhunter)(Sqd4)"})))))
+      (t/is (= (sut/parse-row (first (csv/parse-csv bm-test :delimiter \tab))) {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K"}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv cv-test :delimiter \tab))) {:role "None", :tmm 1, :e* false, :movement {:t 3}, :mul-id 4879, :l* false, :m 5, :type "CV", :abilities "IF2, LRM1/2/2, REAR1/-/-, SRCH, TUR(1/1/1)", :e 0, :s 4, :threshold -1, :l 3, :size 4, :m* false, :point-value 37, :overheat 0, :chassis "Puma Assault Tank", :structure 5, :full-name "Puma Assault Tank PAT-001", :armor 6, :s* false, :model "PAT-001"}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv sv-test :delimiter \tab))) {:role "None", :tmm 4, :e* false, :movement {:h 11}, :mul-id 3684, :l* false, :m 0, :type "SV", :abilities "BAR, EE, ENE", :e 0, :s 0, :threshold -1, :l 0, :size 2, :m* false, :point-value 6, :overheat 0, :chassis "Air Car", :structure 2, :full-name "Air Car ", :armor 1, :s* false, :model ""}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv pm-test :delimiter \tab))) {:role "Scout", :tmm 2, :left-arc "", :e* false, :movement {:walk 6}, :right-arc "", :mul-id 510, :l* false, :m 0, :type "PM", :front-arc "", :abilities "", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* true, :rear-arc "", :point-value 10, :overheat 0, :chassis "Centaur", :structure 1, :full-name "Centaur 2", :armor 1, :s* false, :model "2"}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv ci-test :delimiter \tab))) {:role "Undetermined", :tmm 0, :left-arc "", :e* false, :movement {:s 2}, :right-arc "", :mul-id -1, :l* false, :m 1, :type "CI", :front-arc "", :abilities "AM, CAR4, UMU", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* false, :rear-arc "", :point-value 8, :overheat 0, :chassis "Motorized Sub Platoon", :structure 1, :full-name "Motorized Sub Platoon (Laser SCUBA)", :armor 1, :s* false, :model "(Laser SCUBA)"}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv im-test :delimiter \tab))) {:role "Ambusher", :tmm 1, :left-arc "", :e* false, :movement {:walk 3}, :right-arc "", :mul-id 4545, :l* false, :m 0, :type "IM", :front-arc "", :abilities "BAR, BFC, CT8, EE, ENE, MEL, SAW", :e 0, :s 0, :threshold -1, :l 0, :size 3, :m* false, :rear-arc "", :point-value 7, :overheat 0, :chassis "Lumberjack", :structure 5, :full-name "Lumberjack LM1/A", :armor 1, :s* false, :model "LM1/A"}))
+      (t/is (= (sut/parse-row (first (csv/parse-csv ba-test :delimiter \tab))) {:role "Ambusher", :tmm 1, :left-arc "", :e* false, :movement {:jump 3 :walk 3}, :right-arc "", :mul-id 952, :l* false, :m 0, :type "BA", :front-arc "", :abilities "AM, CAR4, MEC, RCN, RSD1", :e 0, :s 1, :threshold -1, :l 0, :size 1, :m* false, :rear-arc "", :point-value 15, :overheat 0, :chassis "Elemental Battle Armor", :structure 2, :full-name "Elemental Battle Armor (Headhunter)(Sqd4)", :armor 1, :s* false, :model "(Headhunter)(Sqd4)"})))))
 
 (t/deftest test-filters
   (t/testing "Filter by name"
@@ -48,10 +50,42 @@
   (t/testing "Empty filters return unaltered lists."
     (t/is (= (sut/filter-units sut/mul) sut/mul))))
 
+(t/deftest test-print-movement
+  (t/testing "Basic movement."
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K"}) "4"))
+    (t/is (= (sut/print-movement {:role "Test Jumper" :movement {:jump 4 :walk 4}}) "4j/4"))
+    (t/is (= (sut/print-movement {:role "Test Weak Jumper" :movement {:jump 6 :walk 4}}) "6j/4"))
+    (t/is (= (sut/print-movement {:role "Test Strong Jumper" :movement {:jump 2 :walk 4}}) "2j/4")))
+  (t/testing "Damaged movement."
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv]}) "2"))
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :fire-control]}) "2"))
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :mv :fire-control]}) "1"))
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :mv :mv :fire-control]}) "0"))
+    (t/is (= (sut/print-movement {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:fire-control]}) "4"))))
+
+(t/deftest test-tmm 
+  (t/testing "Basic TMM"
+    (t/is (= (sut/get-tmm {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K"}) 1))
+    (t/is (= (sut/get-tmm {:role "High TMM", :tmm 6, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K"}) 6)))
+  (t/testing "Damaged TMM"
+    (t/is (= (sut/get-tmm {:role "Missile Boat", :tmm 1, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv]}) 0)) 
+    (t/is (= (sut/get-tmm {:role "High TMM", :tmm 6, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv]}) 3))
+    (t/is (= (sut/get-tmm {:role "High TMM", :tmm 6, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :mv]}) 2))
+    (t/is (= (sut/get-tmm {:role "High TMM", :tmm 6, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :mv :mv]}) 1))
+    (t/is (= (sut/get-tmm {:role "High TMM", :tmm 6, :e* false, :movement {:walk 4}, :mul-id 73, :l* false, :m 2, :type "BM", :abilities "IF2", :e 0, :s 2, :threshold -1, :l 2, :size 3, :m* false, :point-value 34, :overheat 2, :chassis "Archer", :structure 6, :full-name "Archer ARC-2K", :armor 6, :s* false, :model "ARC-2K" :crits [:mv :mv :mv :mv]}) 0))
+    ))
+
 (t/deftest test-create-element
   (t/testing "Verify new keys merged."
     (t/is (contains? (sut/create-element (first (sut/filter-units sut/mul :full-name "Archer ARC-2K" =))
-                                        {:pilot {:name "Bobby McSkillface" :skill 4}}) :pilot))))
+                                        {:pilot {:name "Bobby McSkillface" :skill 4}}) :pilot)))
+  (t/testing "Test Creation with units"
+    (t/is (= (sut/create-element (sut/get-unit "Wolfhound WLF-2")
+                                 {:path [] :p 11 :q 5 :r -16 :force :1stsomersetstrikers :pilot {:name " Lieutenant Ciro Ramirez", :skill 4} :acted nil :crits [] :current-structure 3 :current-heat 0 :current-armor 4 :movement-mode :walk :direction :s}) 
+             {:role "Striker", :path [], :tmm 2, :q 5, :left-arc "", :e* false, :movement {:walk 6}, :r -16, :right-arc "", :pilot {:name " Lieutenant Ciro Ramirez", :skill 4}, :force :1stsomersetstrikers, :mul-id 3563, :l* false, :m 3, :type "BM", :front-arc "", :current-structure 3, :abilities "ENE, REAR1/1/-", :acted nil, :e 0, :s 3, :threshold -1, :l 1, :size 1, :m* false, :rear-arc "", :point-value 28, :overheat 0, :chassis "Wolfhound", :structure 3, :crits [], :id "Wolfhound WLF-2", :full-name "Wolfhound WLF-2", :armor 4, :current-heat 0, :current-armor 4, :s* false, :p 11, :movement-mode :walk, :direction :s, :model "WLF-2"}))
+ (t/is (= 0 1))
+    ;; This should be expanded much further to handle the edge cases.
+    ))
 
 (t/deftest test-pv-mod-calculation
   (t/testing "Check PV Mod calculation."
@@ -80,6 +114,10 @@
 ;;   (t/testing "Test searching for a valid sprite."
 ;;     (t/is (= (sut/find-sprite {:full-name "Archer ARC-2K" :chassis "Archer"}) "resources/images/units/mechs/Archer_2K.png"))
 ;;     (t/is (= (sut/find-sprite {:full-name "Ahab AHB-4" :chassis "Ahab"}) "resources/images/units/fighter/ahab.png"))))
+
+(t/deftest test-get-units
+  (t/testing "Get units for scenarios."
+    (t/is (= 0 1))))
 
 (t/deftest test-calculate-attacker-mod
   (t/testing "Test valid movement types"
