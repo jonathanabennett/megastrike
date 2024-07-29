0b8577bec106e3b93bb2d59c3c22aad3916ef5a7
Jonathan A. Bennett <doulos05@gmail.com>
2024-07-29 17:21:00 +0900

    Start writing an ability parser

--- src/megastrike/combat_unit.clj
+++ src/megastrike/combat_unit.clj
@@ -77,7 +77,19 @@
    [:right-arc :string]
    [:rear-arc :string]])
 
-
+(defn parse-ability 
+  [str]
+  (cond 
+    (str/includes? "LRM" str) {:ability/type :lrm :s 0 :m 0 :l 0}
+    (str/includes? "ENE" str) {:ability/type :ene}
+    (str/includes? "OMNI" str) {:ability/type :omni}
+    (str/includes? "JMPS" str) {:ability/type :jmps :value 0}
+    (str/includes? "ECM" str) {:ability/type :ecm}
+    (str/includes? "CASE" str) {:ability/type :case}
+    (str/includes? "MEL" str) {:ability/type :mel}
+    (str/includes? "REAR" str) {:ability/type :rear :s 0 :m 0 :l 0}
+    (str/includes? "IF" str) {:ability/type :if :value 0}
+    ))
 
 (defn move-keyword
   "Creates a move keyword from a stat line imported from the mul export."
