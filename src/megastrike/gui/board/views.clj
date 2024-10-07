d92057dc985a48e7adbf6f5f85cf6917c2061062
Jonathan Bennett <doulos05@gmail.com>
2024-10-07 20:53:27 +0900

    Separate attack dialog from draw-unit defn
    First step to moving it out from units completely

--- src/megastrike/gui/board/views.clj
+++ src/megastrike/gui/board/views.clj
@@ -62,22 +62,26 @@
 
 (defn draw-indicator-bar [{:keys [fx/context unit layout]}])
 
+(defn attack-dialog [{:keys [fx/context unit]}]
+  {:fx/type :choice-dialog
+   :showing (fx/sub-val context get-in [:internal (:id unit) :showing] false)
+   :on-close-request (fn [^DialogEvent event]
+                       (when (nil? (.getResult ^Dialog (.getSource event)))
+                         (.consume event)))
+   :header-text "Select Attack"
+   :on-hidden {:event-type ::events/close-attack-selection
+               :unit unit
+               :on-close {:event-type ::events/make-attack
+                          :unit unit}}
+   :items (fx/sub-val context get-in [:internal (:id unit) :items] [])})
+
 (defn draw-unit [{:keys [fx/context unit layout]}]
   (let [hex (hex/points unit layout)
         forces (subs/forces context)
         force (forces (unit :force))]
     {:fx/type fx/ext-let-refs
-     :refs {::dialog {:fx/type :choice-dialog
-                      :showing (fx/sub-val context get-in [:internal (:id unit) :showing] false)
-                      :on-close-request (fn [^DialogEvent event]
-                                          (when (nil? (.getResult ^Dialog (.getSource event)))
-                                            (.consume event)))
-                      :header-text "Select Attack"
-                      :on-hidden {:event-type ::events/close-attack-selection
-                                  :unit unit
-                                  :on-close {:event-type ::events/make-attack
-                                             :unit unit}}
-                      :items (fx/sub-val context get-in [:internal (:id unit) :items] [])}}
+     :refs {::dialog {:fx/type attack-dialog
+                      :unit unit}}
      :desc {:fx/type :group
             :on-mouse-clicked {:event-type ::events/unit-clicked :unit unit}
             :children [{:fx/type common/draw-sprite
@@ -101,27 +105,6 @@
                         :layout-y (nth hex 5)
                         :font 16
                         :translate-y (* (/ (* (layout :y-size) (:scale layout)) 3) -2)}]}}))
-
-(defn draw-target-line [{:keys [fx/context unit layout]}]
-  (let [board (subs/board context)
-        origin-hex (board/find-hex unit board)
-        origin-point (hex/hex->pixel origin-hex layout)
-        target (get (subs/units context) (:target unit))
-        target-hex (board/find-hex target board)
-        target-point (hex/hex->pixel target-hex layout)
-        range (hex/distance unit target)
-        to-hit (attacks/print-attack-roll (attacks/produce-attack-roll unit target board (:attack unit)) false)]
-    {:fx/type :group
-     :children [{:fx/type :line
-                 :start-x (:x origin-point)
-                 :start-y (:y origin-point)
-                 :end-x (:x target-point)
-                 :end-y (:y target-point)}
-                {:fx/type :label
-                 :text (str "Range: " range "; " to-hit "+ To Hit")
-                 :layout-x (/ (+ (:x origin-point) (:x target-point)) 2)
-                 :layout-y (/ (+ (:y origin-point) (:y target-point)) 2)
-                 :font 16}]}))
 
 (defn draw-movement-cost [{:keys [origin destination layout cost]}]
   (let [origin-pixel (hex/hex->pixel origin layout)
@@ -165,10 +148,8 @@
 (defn game-board [{:keys [fx/context]}]
   (let [gb (board/nodes (subs/board context))
         layout (subs/layout context)
-        active-force (first (subs/turn-order context))
         unit-locations (subs/deployed-units context)
-        destinations (filter #(seq (:path %)) (vals (subs/units context)))
-        target-lines (filter #(and (= active-force (:force %)) (:target %)) unit-locations)]
+        destinations (filter #(seq (:path %)) (vals (subs/units context)))]
     {:fx/type :scroll-pane
      :content {:fx/type :group
                :children (concat
@@ -183,10 +164,5 @@
                           (when (seq destinations)
                             (for [t destinations]
                               {:fx/type draw-movement-path
-                               :unit t
-                               :layout layout}))
-                          (when (= (subs/phase context) :combat)
-                            (for [t target-lines]
-                              {:fx/type draw-target-line
                                :unit t
                                :layout layout})))}}))
