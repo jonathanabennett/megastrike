23353b37304e4d47073b56aec8f5c6e0b2f828d7
Jonathan Bennett <doulos05@gmail.com>
2024-10-07 10:07:43 +0900

    Update reports to use get-armor and get-structure methods

--- src/megastrike/gui/reports.clj
+++ src/megastrike/gui/reports.clj
@@ -1,20 +1,21 @@
 (ns megastrike.gui.reports
-  (:require [megastrike.attacks :as attacks]))
+  (:require [megastrike.attacks :as attacks]
+            [megastrike.combat-unit :as cu]))
 
-(defn parse-attack-data 
-  [{:keys [attacker target damage targeting-data to-hit result crit] :as data}]
+(defn parse-attack-data
+  [{:keys [attacker target damage targeting-data to-hit crit]}]
   (let [atk-id (:id attacker)
         tgt-id (:id target)
-        arm (:current-armor target (:armor target))
+        arm (cu/get-armor target)
         penetration (- damage arm)
         target-num (attacks/calculate-to-hit targeting-data)]
     (str atk-id " attacks " tgt-id ". Needs a " target-num ".\n"
-         "Rolled a " (:to-hit data) "\n"
+         "Rolled a " to-hit "\n"
          (if (<= target-num to-hit)
            (str "Attack hits for " damage " damage against " arm " armor.\n"
                 (when (pos? penetration)
-                  (str penetration " damage penetrates. " (:current-structure result) " structure remaining.\n"))
+                  (str penetration " damage penetrates. " (cu/get-structure target) " structure remaining.\n"))
                 (when (or (= to-hit 12) (pos? penetration))
-                  (str "Possible Critical: Rolled " (if crit (name crit) "no critical") " on the critical hits table.\n")))
+                  (str "Possible Critical: Rolled " (if crit (str crit) "no critical") " on the critical hits table.\n")))
            "Attack misses.\n")
-         \newline\newline\newline)))
+         \newline \newline \newline)))
