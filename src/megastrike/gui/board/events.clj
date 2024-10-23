46f5be14a97f9b1e3c345321d55be4a515c00583
Jonathan Bennett <doulos05@gmail.com>
2024-10-23 22:46:47 +0900

    Refactoring board events

--- src/megastrike/gui/board/events.clj
+++ src/megastrike/gui/board/events.clj
@@ -2,8 +2,6 @@
   (:require
    [cljfx.api :as fx]
    [com.brunobonacci.mulog :as mu]
-   [megastrike.attacks :as attacks]
-   [megastrike.combat-unit :as cu]
    [megastrike.gui.events :as events]
    [megastrike.gui.subs :as subs]
    [megastrike.hexagons.hex :as hex]
@@ -11,7 +9,13 @@
   (:import
    [javafx.scene.input MouseEvent]))
 
-;; Add arctan(x2-x1 / y2-y1) formula to handle facing change.
+(defn- facing-change [unit event layout]
+  (let [e ^MouseEvent event
+        u (if (:path unit) (last (:path unit)) unit)
+        dest {:x (.getX e) :y (.getY e)}
+        facing (hex/facing u dest layout)]
+    {:dispatch {:event-type ::events/change-facing :unit unit :facing facing}}))
+
 (defmethod events/event-handler ::hex-clicked
   [{:keys [fx/context hex fx/event]}]
   (let [phase (subs/phase context)
@@ -19,25 +23,16 @@
         units (subs/units context)
         unit (subs/active-unit context)
         next-force (first (subs/turn-order context))]
-    (cond
-      (and (= phase :deployment) (not (nil? unit)) (not (get unit :acted)) (= (get unit :force) next-force))
-      (let [updated (merge unit (select-keys hex [:p :q :r]))
-            new-units (assoc units active updated)]
-        {:context (fx/swap-context context assoc :units new-units)})
-      (and (some #(= phase %) [:deployment :movement]) (not (nil? unit)) (= (fx/sub-val context :turn-flag) true))
-      (let [e ^MouseEvent event
-            u (if (:path unit) (last (:path unit)) unit)
-            dest {:x (.getX e) :y (.getY e)}
-            facing (hex/facing u dest (subs/layout context))]
-        {:dispatch {:event-type ::events/change-facing :unit unit :facing facing}})
-      (and (= phase :movement) (not (nil? unit)) (not (get unit :acted)) (= (get unit :force) next-force))
-      (let [updated (assoc unit :path (movement/find-path unit hex (subs/board context)))
-            new-units (assoc units active updated)]
-        {:context (fx/swap-context context assoc :units new-units)})
-      (and (= phase :combat) (not (nil? unit)))
-      (mu/log ::check-direction
-              :unit unit
-              :hex (hex/hex->offset hex)
-              :facing (:direction unit)
-              :rear (get-in cu/directions [(:direction unit) :rear])
-              :is-behind (attacks/detect-direction unit hex (get-in cu/directions [(:direction unit) :rear]) (fx/sub-val context :layout))))))
+    (if unit
+      (cond
+        (and (= phase :deployment) (not (get unit :acted)) (= (get unit :force) next-force))
+        (let [updated (merge unit (select-keys hex [:p :q :r]))
+              new-units (assoc units active updated)]
+          {:context (fx/swap-context context assoc :units new-units)})
+        (and (some #{phase} [:deployment :movement]) (not (nil? unit)) (fx/sub-val context :turn-flag))
+        (facing-change unit event (subs/layout context))
+        (and (= phase :movement) (not (get unit :acted)) (= (get unit :force) next-force))
+        (let [updated (assoc unit :path (movement/find-path unit hex (subs/board context)))
+              new-units (assoc units active updated)]
+          {:context (fx/swap-context context assoc :units new-units)}))
+      (mu/log ::no-active-unit))))
