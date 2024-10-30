2a0af0f5a08eedad2e14ecb45a09f890aec6b7ab
Jonathan Bennett <doulos05@gmail.com>
2024-10-30 11:56:59 +0900

    Migration from lein to tools.deps

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def class-dir "target/classes")
+(def uber-file (format "target/megastrike.jar"))
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile '[megastrike.core]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main 'megastrike.core}))
