231331d7e9ef70458382085aa06903a876639a4e
Jonathan Bennett <doulos05@gmail.com>
2024-12-09 12:00:43 +0900

    Start of Abilities split
    Required for more complex abilities handling

--- src/megastrike/combat_unit.clj
+++ src/megastrike/combat_unit.clj
@@ -48,20 +48,6 @@
         :points [6 7 8 9]
         :rear :se}})
 
-(defn parse-ability
-  [str]
-  (cond
-    (str/includes? "LRM" str) {:ability/type :lrm :s 0 :m 0 :l 0}
-    (str/includes? "ENE" str) {:ability/type :ene}
-    (str/includes? "OMNI" str) {:ability/type :omni}
-    (str/includes? "JMPS" str) {:ability/type :jmps :value 0}
-    (str/includes? "ECM" str) {:ability/type :ecm}
-    (str/includes? "CASE" str) {:ability/type :case}
-    (str/includes? "MEL" str) {:ability/type :mel}
-    (str/includes? "REAR" str) {:ability/type :rear :s 0 :m 0 :l 0}
-    (str/includes? "IF" str) {:ability/type :if :value 0}
-    (str/includes? "HEAT" str) {:ability/type :heat :s 0 :m 0 :l 0}))
-
 (defn move-keyword
   "Creates a move keyword from a stat line imported from the mul export."
   [mv-type]
