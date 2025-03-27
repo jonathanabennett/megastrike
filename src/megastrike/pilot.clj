f0e97349deb596756a664e63b8f4ed98e0e35725
Jonathan Bennett <doulos05@gmail.com>
2025-03-27 09:34:58 +0900

    Remove unused getters and setters in pilot namespace

--- src/megastrike/pilot.clj
+++ src/megastrike/pilot.clj
@@ -1,37 +1,14 @@
 (ns megastrike.pilot
   "Handles pilots and crews for units.
   
-  `->pilot` creates a pilot from a name and skill
-  `skill` gets the pilot's skill value as an int
-  `fullname` gets the pilot's name
-  `display` prints the pilot's stats as name(skill), so 'Bob Kim(4)'"
-  (:require
-   [clojure.string :as string]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Schema and constructor
-(defn valid-pilot-skill?
-  [n]
-  (<= 0 n 8))
-
-(defn ->pilot
-  ([pname skill kills]
-   (let [pname (string/trim pname)
-         skill (if (= (type skill) java.lang.String)
-                 (Integer/parseInt skill)
-                 skill)
-         pilot {:pilot/name pname :pilot/skill skill :pilot/kills kills}]
-     pilot))
-  ([pilot]
-   (->pilot (:name pilot) (:skill pilot) (get pilot :kills 0))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Querying pilot data
+  `display` prints the pilot's stats as name(skill), so 'Bob Kim(4)'
+  Eventually, this will handle Special Pilot Abilities, which is why I didn't delete it
+  when removing records.")
 
 (defn display
   "Formats the pilot information in the following format: 'Name(skill)'
   Examples:
   Bob Kim(4)
   Shooty McShootyface (2)"
-  [pilot]
-  (str (:pilot/name pilot) " (" (:pilot/skill pilot) ")"))
+  [{:keys [pilot/full-name pilot/skill]}]
+  (str full-name " (" skill ")"))
